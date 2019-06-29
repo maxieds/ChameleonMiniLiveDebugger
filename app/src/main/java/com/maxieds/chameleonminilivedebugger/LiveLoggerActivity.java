9608bf15e781555653aafd2396c64f411c0ef162
maxieds <maxieds@gmail.com / mschmidt34@gatech.edu>
2019-06-29 03:19:02 -0400

    Fixed LiveLoggerActivity.runningActivity == null exceptions

--- app/src/main/java/com/maxieds/chameleonminilivedebugger/LiveLoggerActivity.java
+++ app/src/main/java/com/maxieds/chameleonminilivedebugger/LiveLoggerActivity.java
@@ -137,8 +137,10 @@ public class LiveLoggerActivity extends AppCompatActivity {
                else
                     runningActivity.setStatusIcon(R.id.statusIconNewMsg, R.drawable.statusnewmsg16);
           }
-          logDataFeed.addView(logEntry.getLayoutContainer());
-          logDataEntries.add(logEntry);
+          if(logDataFeed != null && logDataEntries != null) {
+               logDataFeed.addView(logEntry.getLayoutContainer());
+               logDataEntries.add(logEntry);
+          }
           if(LiveLoggerActivity.runningActivity == null) {
                return;
           }
